e69c25cb7f3e6641850901c6b383b5816e99a66e
RaihanKabir <iraihankabir@yahoo.com>
2018-12-05 18:44:28 +0600

    added scroll-top  btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,6 +34,20 @@ $('#scrollToNext').click(function(e){
     }
 });
 
+$('#scrollToTop').click(function(e){
+   e.preventDefault();
+   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: (target.offset().top)
+            }, 1500, "easeInOutExpo");
+            return false;
+        }
+    }
+});
+
 
 
 
