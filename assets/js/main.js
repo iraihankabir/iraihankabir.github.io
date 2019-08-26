1714f3b32e520f70b73f25a41eb9882315772baa
rk4bir <r.kabir01@protonmail.com>
2019-08-26 17:10:16 +0600

    added hire me section

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,6 +57,19 @@ $('#scrollToNext').click(function(e){
     }
 });
 
+$('#scrollToHireMe').click(function(e){
+   e.preventDefault();
+   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: (target.offset().top)
+            }, 1500, "easeInOutExpo");
+            return false;
+        }
+    }
+});
 
 $('#scrollToTop').click(function(e){
    e.preventDefault();
