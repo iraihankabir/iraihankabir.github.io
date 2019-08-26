83da6ecde44f25719762aff62846bf53dac46c99
rk4bir <r.kabir01@protonmail.com>
2019-08-27 03:31:42 +0600

    added loader

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,11 +90,10 @@ $('#scrollToTop').click(function(e){
 $(document).ready(function(){
 
     // check bg img load and stop loader
-    /*$('<img/>').attr('src', 'https://rk4bir.github.io/assets/img/cover.png').on('load',function() {
+    $('<img/>').attr('src', 'https://rk4bir.github.io/assets/img/cover.png').on('load',function() {
        $(this).remove();
        $('#loader').fadeOut();
-    });*/
-    $('#loader').fadeOut();
+    });
 
     say_hello();
     
